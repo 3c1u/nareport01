24485771701c64a90c01807b74117ecf613b9eb4
3c1u <298748+3c1u@users.noreply.github.com>
2019-05-30 11:18:40 +0900

    nalgebra

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-pub mod mat_gen;
-pub mod solver;
+mod mat_gen;
 
+mod nalgebra_solver;
+mod solver;
 use mat_gen::generate_mat;
-use solver::solve;
+
+use nalgebra_solver::solve_na;
+// use solver::solve;
 
 fn main() {
     // A→B方向に並ぶ未知数の数をwidthで与える．
-    // width = 6だと，未知数の数はitems = 21となる．
+    // width = 3だと，未知数の数はitems = 6（問題1），
+    // width = 6だと，未知数の数はitems = 21（問題2）となる．
     let width = 6usize;
     let items = width * (width + 1) / 2;
 
@@ -17,6 +21,14 @@ fn main() {
     ans.resize(items, 0f64);
 
     // 行列式ソルバーに投げる
-    solve(&a, &b, &mut ans).expect("Can't solve");
+    /* nalgebra (LU, width = 70):
+     * 7.61user 0.05system 0:07.75elapsed 98%CPU (0avgtext+0avgdata 99172maxresident)k
+     * 0inputs+0outputs (0major+26907minor)pagefaults 0swaps
+     */
+    /* solve (Gaussの消去法, width = 70):
+     * 8.47user 0.04system 0:08.61elapsed 98%CPU (0avgtext+0avgdata 99384maxresident)k
+     * 0inputs+0outputs (0major+26908minor)pagefaults 0swaps
+     */
+    solve_na(&a, &b, &mut ans).expect("Can't solve");
     println!("{:#?}", ans);
 }
---
+++ src/nalgebra_solver.rs
@@ -0,0 +1,15 @@
+// nalgebra（LAPACKのラッパー）を用いて解く方法
+
+use nalgebra as na;
+
+pub fn solve_na<T: AsRef<[f64]>>(a: &[T], b: &[f64], ans: &mut [f64]) -> Option<()> {
+  let a = na::DMatrix::from_fn(a.len(), a.len(), |r, c| a[r].as_ref()[c]);
+  let b = na::DVector::from_row_slice(b);
+
+  let lu = na::LU::new(a);
+  let ansv = lu.solve(&b)?;
+
+  ans.clone_from_slice(ansv.as_slice());
+
+  Some(())
+}
